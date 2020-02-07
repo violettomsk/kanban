752f0bff13c08608ce6c9780e0b5a9354029063e
ywm.nhan <nhan@ymwobile.co.kr>
2020-02-07 17:04:01 +0700

    translate to en

--- init.rb
+++ init.rb
@@ -7,10 +7,10 @@ Redmine::Plugin.register :kanban do
   author_url 'mailto:kohei_nom@yahoo.co.jp'
   
   #アプリケーションメニューに「かんばん」を表示
-  menu :application_menu, :kanban, { :controller => 'kanban', :action => 'index' }, :caption => 'かんばん', :if => Proc.new { User.current.logged? }
+  menu :application_menu, :kanban, { :controller => 'kanban', :action => 'index' }, :caption => 'KANBAN', :if => Proc.new { User.current.logged? }
   
   #プロジェクトメニューに「かんばん」を表示
-  menu :project_menu, :kanban, { :controller => 'kanban', :action => 'index' }, :caption => 'かんばん', :param => :project_id
+  menu :project_menu, :kanban, { :controller => 'kanban', :action => 'index' }, :caption => 'KANBAN', :param => :project_id
   
   # プロジェクトごとの権限を追加
   project_module :kanban do
